2c0288ccc5ab44bb9bf81f4d881b081c8c7f4e46
Waleed Alharbi <waleed1417@gmail.com>
2020-06-10 21:49:40 +0300

    Create ipsubtract.py

---
+++ ipsubtract.py
@@ -0,0 +1,18 @@
+from ipaddress import ip_network
+
+start = '0.0.0.0/0'
+exclude = ['192.168.1.0/24','192.168.254.0/24']
+
+result = [ip_network(start)]
+
+for x in exclude:
+    n = ip_network(x)
+    new = []
+    for y in result:
+        if y.overlaps(n):
+            new.extend(y.address_exclude(n))
+        else:
+            new.append(y)
+    result = new
+
+print(','.join(str(x) for x in sorted(result)))
